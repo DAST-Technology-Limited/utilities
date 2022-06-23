6bf029daa74396192d8252a7ab85e317cc1527b9
iyidaclem <iyidaclem@gmail.com>
2022-06-23 10:35:20 +0100

    First commit

---
+++ resources/views/instruction.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instruction</title>
+</head>
+<body>
+    <h1>Build the following apis</h1>
+    <ol>
+        <li><h1>Mailing Api</h1></li>
+        <li>
+            <ul>
+                <li>
+                    It accepts the title
+                </li>
+                <li>Body</li>
+                <li>Cover image</li>
+                <li>Receiver</li>
+                <li>Sends the message to the </li>
+            </ul>
+        </li>
+        <li>
+            Your can checkout php mailer for
+        </li>
+    </ol>
+</body>
+</html>
