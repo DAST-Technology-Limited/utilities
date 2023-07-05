42066acead51079374dc7fbf71d95a6576a5a2cc
iyidaclem <iyidaclem@gmail.com>
2023-07-05 16:32:09 +0100

    saved

--- app/Http/Controllers/Funding/VellaFinanceController.php
+++ app/Http/Controllers/Funding/VellaFinanceController.php
@@ -21,8 +21,8 @@ class VellaFinanceController extends Controller
     public function vellaWebHook(Request $request)
     {
         if ($request->type == "transaction.completed" && $request->data["status"] == "successful") {
-            $meta_data = json_decode($request->data["meta_data"]);
-            $trans = VellaFinance::where("payment_id", $meta_data->id)->where("status", Status::PENDING())->where("amount", $request->data->total)->first();
+            $meta_data = $request->data["meta_data"];
+            $trans = VellaFinance::where("payment_id", $meta_data["id"])->where("status", Status::PENDING())->where("amount", $request->data["total"])->first();
 
             if ($trans) {
                 $trans->status = Status::APPROVED();
