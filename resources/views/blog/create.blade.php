a830856bc79709d5a7bcd4115dd770ad82d0f04b
iyidaclem <iyidaclem@gmail.com>
2023-07-07 22:32:01 +0100

    saved

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 @extends('blog.app')
 
 @section('content')
@@ -14,7 +8,7 @@
 <html>
 
 <head>
-  
+
   <style>
     body {
       font-family: Arial, sans-serif;
@@ -67,144 +61,159 @@
       input[type="submit"] {
         width: 100%;
       }
-  
+
       input[type="submit"]:hover {
         background-color: #45a049;
       }
-  
+
       /* Responsive styles */
       @media screen and (max-width: 600px) {
         form {
           max-width: 100%;
           padding: 20px;
         }
-  
+
         input[type="submit"] {
           width: 100%;
         }
       }
+  </style>
+
+  <script src="https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.css">
+
+  <script src="https://cdn.tiny.cloud/1/t1z5rqxi98ch2jc0vuqycqv4aniu58hp58fty93l20n17ynx/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+</head>
+
+<body>
+  <section class="about container" id="about" style="margin-top:1rem;">
+
+    <div class="imgBx" style="margin-top:1rem;">
+      <img src="https://media.istockphoto.com/id/1425936006/photo/influencer-reviewing-her-live-broadcast-schedule.webp?b=1&s=170667a&w=0&k=20&c=ehFBnmRqVnrByCyTFrYO281sOqSCs2dIbUd5WcPjqIw=" alt="" class="fitBg">
+    </div>
+  </section>
+
+
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.16/css/bootstrap-multiselect.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.16/js/bootstrap-multiselect.min.js"></script>
+
+
+  <form id="create" action="/store" method="post" enctype="multipart/form-data">
+    @csrf
+    @if (session('message'))
+    <script>
+      toastr.success('{{ session('
+        message ') }}', '', {
+          timeOut: 3000
+        });
+    </script>
+    @endif
+
+
+    <script>
+      toastr.options = {
+        closeButton: true,
+        progressBar: true,
+        positionClass: 'toast-top-right',
+        preventDuplicates: false,
+        showDuration: 300,
+        hideDuration: 1000,
+        timeOut: 5000,
+        extendedTimeOut: 1000,
+        showEasing: 'swing',
+        hideEasing: 'linear',
+        showMethod: 'fadeIn',
+        hideMethod: 'fadeOut'
+      };
+    </script>
+    <a href="/categories/create">
+      <h5>Create Category</h5>
+    </a>
+    <label for="title">Blog Title:</label>
+    <input type="text" id="title" name="title" required><br><br>
+
+    <label for="author">Blog author:</label>
+    <input type="text" id="author" name="author" required><br><br>
+
+    <label for="category_id">Categories:</label>
+    <div class="checkbox-container">
+      @foreach($categories as $category)
+      <div class="category-item">
+        <input type="checkbox" name="category_id[]" id="category_{{ $category->id }}" value="{{ $category->id }}">
+        <label for="category_{{ $category->id }}">{{ $category->name }}</label>
+      </div>
+      @endforeach
+    </div>
+
+    <style>
+      .checkbox-container {
+        display: flex;
+        flex-wrap: wrap;
+      }
+
+      .category-item {
+        display: flex;
+        align-items: center;
+        margin-right: 20px;
+      }
+
+      .category-item input[type="checkbox"] {
+        margin-right: 5px;
+      }
+
+      .category-item label {
+        font-weight: bold;
+      }
+
+      /* Responsive Styles */
+      @media screen and (max-width: 768px) {
+        .category-item {
+          margin-right: 10px;
+        }
+      }
+
+      @media screen and (max-width: 480px) {
+        .category-item {
+          margin-right: 5px;
+        }
+      }
     </style>
 
-<script src="https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.css">
 
-  </head>
-  <body>
-    <section class="about container" id="about" style="margin-top:1rem;">
-      
-      <div class="imgBx" style="margin-top:1rem;">
-        <img src="https://media.istockphoto.com/id/1425936006/photo/influencer-reviewing-her-live-broadcast-schedule.webp?b=1&s=170667a&w=0&k=20&c=ehFBnmRqVnrByCyTFrYO281sOqSCs2dIbUd5WcPjqIw=" alt="" class="fitBg">
-      </div>
-    </section>
+    <label for="details">Blog Details:</label>
+    <textarea id="details" name="body" required></textarea><br><br>
 
+    <label for="file">Upload File:</label>
+    <input type="file" id="file" name="image"><br><br>
 
-   
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.16/css/bootstrap-multiselect.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.16/js/bootstrap-multiselect.min.js"></script>
-    
-
-    <form id="create" action="/store" method="post" enctype="multipart/form-data">
-      @csrf
-      @if (session('message'))
-      <script>
-          toastr.success('{{ session('message') }}', '', { timeOut: 3000 });
-      </script>
-  @endif
-
-  
-  <script>
-    toastr.options = {
-    closeButton: true,
-    progressBar: true,
-    positionClass: 'toast-top-right',
-    preventDuplicates: false,
-    showDuration: 300,
-    hideDuration: 1000,
-    timeOut: 5000,
-    extendedTimeOut: 1000,
-    showEasing: 'swing',
-    hideEasing: 'linear',
-    showMethod: 'fadeIn',
-    hideMethod: 'fadeOut'
-};
-
-  </script>
-      <a href="/categories/create"><h5>Create Category</h5></a>
-      <label for="title">Blog Title:</label>
-      <input type="text" id="title" name="title" required><br><br>
-
-      <label for="author">Blog author:</label>
-      <input type="text" id="author" name="author" required><br><br>
-
-      <label for="category_id">Categories:</label>
-      <div class="checkbox-container">
-        @foreach($categories as $category)
-          <div class="category-item">
-            <input type="checkbox" name="category_id[]" id="category_{{ $category->id }}" value="{{ $category->id }}">
-            <label for="category_{{ $category->id }}">{{ $category->name }}</label>
-          </div>
-        @endforeach
-      </div>
-      
-<style>
-  .checkbox-container {
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.category-item {
-  display: flex;
-  align-items: center;
-  margin-right: 20px;
-}
-
-.category-item input[type="checkbox"] {
-  margin-right: 5px;
-}
-
-.category-item label {
-  font-weight: bold;
-}
-
-/* Responsive Styles */
-@media screen and (max-width: 768px) {
-  .category-item {
-    margin-right: 10px;
-  }
-}
-
-@media screen and (max-width: 480px) {
-  .category-item {
-    margin-right: 5px;
-  }
-}
-
-</style>
-      
-
-      <label for="details">Blog Details:</label>
-      <textarea id="details" name="body" required></textarea><br><br>
-
-      <label for="file">Upload File:</label>
-      <input type="file" id="file" name="image"><br><br>
-
-      <input type="submit" value="Submit"  style="background: rgb(1, 1, 54);color:white;" >
+    <input type="submit" value="Submit" style="background: rgb(1, 1, 54);color:white;">
   </form>
 
+  <script>
+    tinymce.init({
+      selector: 'textarea',
+      plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+      setup: function(editor) {
+        editor.on('change', function(e) {
+          editor.save();
+        });
+      }
+    });
+  </script>
 
+  <script>
+    $(document).ready(function() {
+      $('.multiselect').multiselect({
+        buttonWidth: '200px',
+        enableFiltering: true,
+        includeSelectAllOption: true
+      });
+    });
+  </script>
 
-<script>
-  $(document).ready(function() {
-  $('.multiselect').multiselect({
-    buttonWidth: '200px',
-    enableFiltering: true,
-    includeSelectAllOption: true
-  });
-});
-
-</script>
-
-@endsection
+  @endsection
